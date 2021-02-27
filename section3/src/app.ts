2053334b9c7a52f68f60e84d1173ea3d8fdba690
kamimi01 <awakaru0122@gmail.com>
2021-02-27 23:04:34 +0900

    feat: セクション3の完了

---
+++ section3/src/app.ts
@@ -0,0 +1,18 @@
+// DOMの機能が使えるので、querySelectorなどがエラーなく使える
+const button = document.querySelector("button");
+
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+  return;
+}
+
+function clickHandler(message: string) {
+  // let userName = "Max";
+  console.log("Clicked!" + message);
+}
+
+if (button) {
+  button.addEventListener("click", clickHandler.bind(null, "you are welcome"));
+}
